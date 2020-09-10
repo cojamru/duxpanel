7b0822f819539a7b69880a02430baa151bda0d6a
Dmitry <dmitry@cojam.ru>
2020-09-10 17:25:52 +0500

    addes source maps, make ./src available in prod build
    + added types for lodash.throttle

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,22 @@ const clean = require('gulp-clean');
 const cleanCSS = require('gulp-clean-css');
 const terser = require('gulp-terser');
 const ts = require('gulp-typescript');
+const sourceMaps = require('gulp-sourcemaps');
 
 var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('clean-dist', () => pipe([gulp.src('lib', { read: false, allowEmpty: true }), clean()]));
 
-gulp.task('build-js', () => pipe([gulp.src(['src/**/*.js', 'src/**/*.jsx']), tsProject(), terser(), gulp.dest('lib')]));
+gulp.task('build-js', () =>
+    pipe([
+        gulp.src(['src/**/*.js', 'src/**/*.jsx']),
+        sourceMaps.init(),
+        tsProject(),
+        terser(),
+        sourceMaps.write('.', { sourceRoot: 'src', includeContent: false }),
+        gulp.dest('lib'),
+    ])
+);
 
 gulp.task('build-css', () => pipe([gulp.src('src/**/*.css'), cleanCSS(), gulp.dest('lib')]));
 
