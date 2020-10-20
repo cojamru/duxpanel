5e7bc12a6c6ee4ac4390f25eecb3eaf425bf8622
Dmitry <dmitry@cojam.ru>
2020-10-20 12:48:05 +0500

    сделал очистку директории dist

--- webpack.website-dist.js
+++ webpack.website-dist.js
@@ -4,13 +4,16 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
+
+const OutputPath = path.resolve(__dirname, 'website/dist');
 
 module.exports = {
     entry: './website/index.tsx',
     mode: 'production',
     output: {
-        path: path.resolve(__dirname, 'website/dist'),
-        filename: 'bundle.js',
+        path: OutputPath,
+        filename: '[name].bundle.js',
     },
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
@@ -19,6 +22,11 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new RemovePlugin({
+            before: {
+                include: [OutputPath],
             },
         }),
         new MiniCssExtractPlugin({
@@ -36,6 +44,9 @@ module.exports = {
         usedExports: true,
         minimize: true,
         minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+        splitChunks: {
+            chunks: 'all',
+        },
     },
     module: {
         rules: [
